c09233fe058dc2211748e74273b8a0af74be98a1
Alyson Takai <shtakai@gmail.com>
2016-07-07 19:22:37 -0700

    wip

--- test/dojoquery_test.js
+++ test/dojoquery_test.js
@@ -18,11 +18,15 @@ describe('dojoquery', function(){
    expect($Dojoquery('button').html()).to.match(/<button><\/button>/);
   });
 
-  it('respond to click', function(){
-     expect($Dojoquery('button')).to.respondTo('click');
+
+  it('respond to click with one callback', function(){
+    var callback = function(){return 'clicked';}
+     expect($Dojoquery('button').click(callback)).to.match(/<button><\/button>/);
   });
 
-  it('respond to hover', function(){
-     expect($Dojoquery('button')).to.respondTo('hover');
+  it('respond to hover with two callback', function(){
+    var callback1 = function(){return 'hover in';}
+    var callback2 = function(){return 'hover out';}
+     expect($Dojoquery('button').click(callback1, callback2)).to.not.throw(Error);
   });
 });
